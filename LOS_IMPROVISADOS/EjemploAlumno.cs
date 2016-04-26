502b56a70136a15b21e2d0b3e66b4849cbf4ae93
alexiscaspell <alexiscaspell@gmail.com>
2016-04-26 20:19:08 -0300

    otro cambio

--- LOS_IMPROVISADOS/EjemploAlumno.cs
+++ LOS_IMPROVISADOS/EjemploAlumno.cs
@@ -40,7 +40,7 @@ namespace AlumnoEjemplos.MiGrupo
 
         public override void init()
         {
-            Device d3dDevice = GuiController.Instance.D3dDevice;//GG VIEJA!!
+            Device d3dDevice = GuiController.Instance.D3dDevice;
             
             TgcSceneLoader loader = new TgcSceneLoader();
             tgcEscena = loader.loadSceneFromFile(
