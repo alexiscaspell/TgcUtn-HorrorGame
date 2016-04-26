ecbf7296e611c7f3ab47630993e973e12f4f6c1e
alexiscaspell <alexiscaspell@gmail.com>
2016-04-25 21:12:11 -0300

    Primer commit

---
+++ AlumnoEjemplos/LOS_IMPROVISADOS/Bateria.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.DirectX;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TgcViewer;
+using TgcViewer.Utils._2D;
+using TgcViewer.Utils.TgcSceneLoader;
+
+namespace AlumnoEjemplos.LOS_IMPROVISADOS
+{
+    class Bateria
+    {
+        //private Vector2 posicion;
+
+        private TgcSprite bateriaActual;
+
+        private List<TgcSprite> listaBaterias;
+
+        private int cantBateria;
+
+        private System.DateTime tiempoAnterior;
+
+        private int velocidadDePerdida;
+
+        public void init(int velocidadPerdida)
+        {
+            cantBateria = 4;
+
+            velocidadDePerdida = velocidadPerdida;
+
+            inicializarBaterias();
+
+            bateriaActual = listaBaterias.ElementAt(cantBateria);
+
+            tiempoAnterior = System.DateTime.Now;
+            
+        }
+
+        private void inicializarBaterias()
+        {
+            TgcSprite sprite1 = new TgcSprite();
+            sprite1.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "Media\\Texturas\\bateria1.png");
+            TgcSprite sprite2 = new TgcSprite();
+            sprite2.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "Media\\Texturas\\bateria2.png");
+            TgcSprite sprite3 = new TgcSprite();
+            sprite3.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "Media\\Texturas\\bateria3.png");
+            TgcSprite sprite4 = new TgcSprite();
+            sprite4.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "Media\\Texturas\\bateria4.png");
+            TgcSprite sprite5 = new TgcSprite();
+            sprite5.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "Media\\Texturas\\bateria5.png");
+
+            //Ubicarlo centrado en la pantalla
+            Size screenSize = GuiController.Instance.Panel3d.Size;
+
+            sprite1.Scaling = new Vector2((float)0.0002 * screenSize.Width, (float)0.0005 * screenSize.Height);
+            sprite2.Scaling = new Vector2((float)0.0002 * screenSize.Width, (float)0.0005 * screenSize.Height);
+            sprite3.Scaling = new Vector2((float)0.0002 * screenSize.Width, (float)0.0005 * screenSize.Height);
+            sprite4.Scaling = new Vector2((float)0.0002 * screenSize.Width, (float)0.0005 * screenSize.Height);
+            sprite5.Scaling = new Vector2((float)0.0002 * screenSize.Width, (float)0.0005 * screenSize.Height);
+
+            sprite1.Position = new Vector2(0, 20);
+            sprite2.Position = new Vector2(0, 20);
+            sprite3.Position = new Vector2(0, 20);
+            sprite4.Position = new Vector2(0, 20);
+            sprite5.Position = new Vector2(0, 20);
+
+            listaBaterias = new List<TgcSprite>();
+
+            listaBaterias.Add(sprite1);
+            listaBaterias.Add(sprite2);
+            listaBaterias.Add(sprite3);
+            listaBaterias.Add(sprite4);
+            listaBaterias.Add(sprite5);
+            
+        }
+
+        public void render()
+        {
+            TimeSpan tiempoTranscurrido = DateTime.Now.Subtract(tiempoAnterior);
+
+            if (tiempoTranscurrido.Seconds.Equals(velocidadDePerdida)&&cantBateria>0)
+            {
+                cantBateria--;
+                tiempoAnterior = DateTime.Now;
+                bateriaActual = listaBaterias.ElementAt(cantBateria);
+            }
+
+            GuiController.Instance.Drawer2D.beginDrawSprite();        
+            bateriaActual.render();           
+            GuiController.Instance.Drawer2D.endDrawSprite();
+        }
+    }
+}
---
+++ AlumnoEjemplos/LOS_IMPROVISADOS/CamaraFPS.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TgcViewer;
+using Microsoft.DirectX;
+using TgcViewer.Utils.TgcSceneLoader;
+
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using TgcViewer.Utils.TgcGeometry;
+
+namespace AlumnoEjemplos.LOS_IMPROVISADOS
+{
+    class CamaraFPS
+    {
+        public Vector3 posicion { get; set; }
+
+        public Vector3 direccionVista { get; set; }
+
+        public CamaraFramework camaraFramework { get; set; }
+
+        public CamaraFPS(Vector3 posicionInicial, Vector3 direccionVistaInicial)
+        {
+            this.posicion = posicionInicial;
+            this.direccionVista = direccionVistaInicial;
+
+            camaraFramework = new CamaraFramework();
+        }
+        
+        public void init()
+        {
+            crearModificadores();
+            crearVariablesDeUsuario();
+
+            camaraFramework.Enable = true;
+            camaraFramework.setCamera(posicion, direccionVista);
+            camaraFramework.JumpSpeed = 200f;
+            /*
+            GuiController.Instance.FpsCamera.Enable = true;
+            GuiController.Instance.FpsCamera.setCamera(posicion, direccionVista);
+            GuiController.Instance.FpsCamera.JumpSpeed = 200f;
+            */
+        }
+
+        public void render()
+        {
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+            usarModificadoresIniciales();
+            usarVariablesDeUsuarioIniciales();
+
+            camaraFramework.updateCamera();
+            camaraFramework.updateViewMatrix(d3dDevice);
+        }
+                
+        public void crearVariablesDeUsuario()
+        {
+            GuiController.Instance.UserVars.addVar("camaraX");
+            GuiController.Instance.UserVars.addVar("camaraY");
+            GuiController.Instance.UserVars.addVar("camaraZ");
+
+            GuiController.Instance.UserVars.addVar("vistaX");
+            GuiController.Instance.UserVars.addVar("vistaY");
+            GuiController.Instance.UserVars.addVar("vistaZ");
+
+        }
+
+        public void crearModificadores()
+        {
+            GuiController.Instance.Modifiers.addFloat("velocidadCaminar", 0f, 400f, 40f);
+        }
+
+        public void usarVariablesDeUsuarioIniciales()
+        {
+            GuiController.Instance.UserVars.setValue("camaraX", GuiController.Instance.FpsCamera.getPosition().X);
+            GuiController.Instance.UserVars.setValue("camaraY", GuiController.Instance.FpsCamera.getPosition().Y);
+            GuiController.Instance.UserVars.setValue("camaraZ", GuiController.Instance.FpsCamera.getPosition().Z);
+
+            GuiController.Instance.UserVars.setValue("vistaX", GuiController.Instance.FpsCamera.getLookAt().X);
+            GuiController.Instance.UserVars.setValue("vistaY", GuiController.Instance.FpsCamera.getLookAt().Y);
+            GuiController.Instance.UserVars.setValue("vistaZ", GuiController.Instance.FpsCamera.getLookAt().Z);
+        }
+
+        public void usarModificadoresIniciales()
+        {
+            //GuiController.Instance.FpsCamera.MovementSpeed = (float)GuiController.Instance.Modifiers["velocidadCaminar"];
+            camaraFramework.MovementSpeed = (float)GuiController.Instance.Modifiers["velocidadCaminar"];
+        }
+
+    }
+}
